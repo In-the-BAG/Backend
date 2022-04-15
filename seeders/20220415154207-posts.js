62756fd7f4760722c8638791216aee4454e3a0f6
GJ Haaland <haaland.gj@gmail.com>
2022-04-15 12:22:35 -0400

    controllers

--- seeders/20220415154207-posts.js
+++ seeders/20220415154207-posts.js
@@ -4,26 +4,20 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('posts', [
       {
-        
+        image: 'https://media.npr.org/assets/img/2021/10/19/kanyewest_wide-96a94105571576fccf8dac36615998c2ddf87634.jpg?s=1400',
+        description: 'this is a test',
+        userId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+
       }
-    ])
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    ],
+    {}
+    )
+   
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('posts', null, {})
   }
 };
