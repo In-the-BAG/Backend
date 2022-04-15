facf94776a36a3ec295495ed6084086bb21c3176
GJ Haaland <haaland.gj@gmail.com>
2022-04-15 11:34:47 -0400

    AppRouter

---
+++ routes/AppRouter.js
@@ -0,0 +1,11 @@
+const Router = require('express').Router()
+
+const UserRouter = require('./UserRoute')
+const PostRouter = require('./PostRoute')
+const CommentRouter = require('./CommentRoute')
+
+Router.use('/user', UserRouter)
+Router.use('/post', PostRouter)
+Router.use('/comment', CommentRouter)
+
+module.exports = Router
