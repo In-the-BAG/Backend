51f5d8146ee0b8666cfa1bf02e66ceb4c049122f
GJ Haaland <haaland.gj@gmail.com>
2022-04-15 15:02:31 -0400

    server up and running

--- routes/AppRouter.js
+++ routes/AppRouter.js
@@ -1,11 +1,11 @@
 const Router = require('express').Router()
 
 const UserRouter = require('./UserRoute')
-const PostRouter = require('./PostRoute')
-const CommentRouter = require('./CommentRoute')
+// // const PostRouter = require('./PostRoute')
+// // const CommentRouter = require('./CommentRoute')
 
-Router.use('/user', UserRouter)
-Router.use('/post', PostRouter)
-Router.use('/comment', CommentRouter)
+Router.use('/users', UserRouter)
+// // Router.use('/post', PostRouter)
+// // Router.use('/comment', CommentRouter)
 
 module.exports = Router
