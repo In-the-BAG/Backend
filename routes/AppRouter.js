1e749f56269b4e8ab4d77127471848d1fa2041aa
Alex Barbosa <alex.andrew.barbosa@gmail.com>
2022-04-18 09:36:34 -0400

    create post now functional

--- routes/AppRouter.js
+++ routes/AppRouter.js
@@ -1,11 +1,11 @@
 const Router = require('express').Router()
 
 const UserRouter = require('./UserRoute')
-// const PostRouter = require('./PostRouter')
+const PostRouter = require('./PostRouter')
 const CommentRouter = require('./CommentRoute')
 
 Router.use('/user', UserRouter)
-// Router.use('/post', PostRouter)
+Router.use('/post', PostRouter)
 Router.use('/comment', CommentRouter)
 
 module.exports = Router
