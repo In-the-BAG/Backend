9ca3ef2d674fb070ba35dd938a8482c4fb738707
GJ Haaland <haaland.gj@gmail.com>
2022-04-19 12:11:05 -0400

    turned on userRoutes

--- routes/CommentRoute.js
+++ routes/CommentRoute.js
@@ -1,10 +1,10 @@
 const Router = require('express').Router()
 const controller = require('../controllers/CommentController')
 
-// Router.get('/allComments', controller.GetAllComments),
-// Router.get('/view/:commentid', controller.GetCommentDetails),
-// Router.post('/:userid/:postid', controller.CreateComment)
-// Router.put('/:commentid', controller.UpdateComment)
-// Router.delete('/:commentid', controller.DeleteComment)
+Router.get('/allComments', controller.GetAllComments),
+Router.get('/view/:commentid', controller.GetCommentDetails),
+Router.post('/:userid/:postid', controller.CreateComment)
+Router.put('/:commentid', controller.UpdateComment)
+Router.delete('/:commentid', controller.DeleteComment)
 
 module.exports = Router
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express()
 const AppRouter = require('./routes/AppRouter')
 const AuthRouter = require('./routes/AuthRouter')
 const PostRouter = require('./routes/PostRouter')
+const UserRouter = require('./routes/UserRoute')
 
 const PORT = process.env.PORT || 3023
 
@@ -18,5 +19,6 @@ app.get('/', (req, res) => res.json({ message: 'Server Works' }))
 app.use('/api', AppRouter)
 app.use('/auth', AuthRouter)
 app.use('/post', PostRouter)
+app.use('/user', UserRouter)
 
 app.listen(PORT, () => console.log(`Server Started On Port: ${PORT}`))
